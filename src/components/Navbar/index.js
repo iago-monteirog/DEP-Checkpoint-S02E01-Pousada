cdb1ed2fd1825ab78cb14977635c142503318cec
Abraão Azevedo <az.abraao@gmail.com>
2020-08-28 21:04:39 -0300

    add navbar, feather icons

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { Menu } from "react-feather";
 
 export default function Navbar() {
-  return <nav>navbar</nav>;
+  return (
+    <nav className="Navbar">
+      <div className="container">
+        <div className="Navbar__logo"></div>
+        <div className="Navbar__hamburguer-menu">
+          <Menu />
+        </div>
+        <div className="MenuLinks">
+          <div className="MenuLinks__container">
+            <div className="MenuLinks__link">
+              <a href="#">Pagina inicial</a>
+            </div>
+            <div className="MenuLinks__link">
+              <a href="#">Sobre</a>
+            </div>
+            <div className="MenuLinks__link">
+              <a href="#">Quartos</a>
+            </div>
+            <div className="MenuLinks__link">
+              <a href="#">Contato</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
