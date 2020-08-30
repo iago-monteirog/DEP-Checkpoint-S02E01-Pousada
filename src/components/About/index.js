bcbf457f9a751cea5ee795bddee2ae0b3407ee0f
Abraão Azevedo <az.abraao@gmail.com>
2020-08-29 22:09:44 -0300

    add about section

---
+++ src/components/About/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <article className="About Content">
+      <div className="container">
+        <div className="About__inside">
+          <h2>Sobre</h2>
+          <div className="Content__columns">
+            <article>
+              <p>
+                Como cidade capital, abriga na zona chamada Plateau, promontório
+                à beira-mar, edifícios públicos e outras construções de
+                importância, como o Palácio Presidencial, construído no fim do
+                século XIX para ser a residência do governador português.
+              </p>
+              <p>
+                Como cidade capital, abriga na zona chamada Plateau, promontório
+                à beira-mar, edifícios públicos e outras construções de
+                importância, como o Palácio Presidencial, construído no fim do
+                século XIX para ser a residência do governador português.
+              </p>
+            </article>
+            <article>
+              <p>
+                Como cidade capital, abriga na zona chamada Plateau, promontório
+                à beira-mar, edifícios públicos e outras construções de
+                importância, como o Palácio Presidencial, construído no fim do
+                século XIX para ser a residência do governador português.
+              </p>
+              <p>
+                Como cidade capital, abriga na zona chamada Plateau, promontório
+                à beira-mar, edifícios públicos e outras construções de
+                importância, como o Palácio Presidencial, construído no fim do
+                século XIX para ser a residência do governador português.
+              </p>
+            </article>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,13 +3,15 @@ import "./Home.scss";
 import Navbar from "../../components/Navbar";
 import HomeBanner from "../../components/HomeBanner";
 import BoxReserva from "../../components/BoxReserva";
+import About from "../../components/About";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <HomeBanner />
-      <BoxReserva/>
+      <BoxReserva />
+      <About/>
     </>
   );
 }
