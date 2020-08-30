24b400e9c377fb39e4e0fd1bb7a37cd382c138ff
Abraão Azevedo <az.abraao@gmail.com>
2020-08-30 10:06:19 -0300

    order things in public folder

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
-import React  from "react";
+import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Quartos from "./pages/Quartos";
 
 export default function Routes() {
   return (
@@ -8,6 +9,7 @@ export default function Routes() {
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/quartos" exact component={Quartos} />
         </Switch>
       </BrowserRouter>
     </>
